87fd22bb5f441e56c7b729693c078a075403addd
Javier Estevez <javierdaniloestevez@gmail.com>
2022-04-16 03:16:43 -0300

    creando el servidor web con Express y probando metodo get

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3789;
 
 mongoose.connect('mongodb://localhost:27017/refugio', (err, resp) => {
     if (err)
         throw err;
-    else
+    else {
         console.log('La conexión con la base refugio se hizo correctamente!');
+        app.listen(port, () => {
+            console.log('El servidor local con Node y Express esta ejecutandose');
+        });
+    }
 });
