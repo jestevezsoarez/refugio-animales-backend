eb8254e18df2db4c6aa36f1d129d0f2836f16c63
Javier Estevez <javierdaniloestevez@gmail.com>
2022-04-24 03:48:10 -0300

    creo 2 metodos para subir imagen y obtener imagen de animal

--- controllers/animal.js
+++ controllers/animal.js
@@ -105,11 +105,75 @@ function updateAnimal(req, res) {
     });
 }
 
+function uploadImage(req, res) {
+    var animalId = req.params.id;
+    var file_name = 'No subido...';
+
+    // compruebo que me llegan archivos
+    if (req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+
+            Animal.findByIdAndUpdate(animalId, {image: file_name}, {new: true}, (err, animalUpdated) => {
+                if (err) {
+                    res.status(500).send({
+                        message: 'Error al actualizar usuario'
+                    });
+                } else {
+                    if (!animalUpdated) {
+                        res.status(404).send({
+                            message: 'No se ha podido actualizar el animal'
+                        });
+                    } else {
+                        res.status(200).send({animal: animalUpdated, image: file_name});
+                    }
+                }
+            });
+        } else {
+            fs.unlink(file_path, (err) => {
+                if (err) {
+                    res.status(200).send({message: 'Extension no valida y archivo no borrado'});
+                } else {
+                    res.status(200).send({message: 'Extension no valida'});
+                }
+            });
+        }
+    } else {
+        res.status(200).send({message: 'No se han subido archivos'});
+    }
+}
+
+function getImageFile(req, res) {
+    var imageFile = req.params.imageFile;    
+    var pathFile = './uploads/animals/' + imageFile;
+
+    res.sendFile(path.resolve(pathFile), function (err) {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.status(200).send({message: 'No existe la imagen'});
+            }
+            else {
+                // handle other errors...
+            }
+        }
+    });
+}
+
+
+
 
 module.exports = {
     pruebas,
     saveAnimal,
     getAnimals,
     getAnimal,
-    updateAnimal
+    updateAnimal,
+    uploadImage,
+    getImageFile
 };
