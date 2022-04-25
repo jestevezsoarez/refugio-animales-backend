0f8148b5bd00f5731ccc50ca8451405121f048be
Javier Estevez <javierdaniloestevez@gmail.com>
2022-04-25 05:11:55 -0300

    creo el CORS de la api

--- app.js
+++ app.js
@@ -15,8 +15,14 @@ app.use(bodyParser.json());
 
 
 // configurar cabeceras y cors
-
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+ 
 // rutas base
 app.use('/api', user_routes); // le pongo el prefijo /api, sino quiero prefijo pongo solo /
 app.use('/api', animal_routes);
